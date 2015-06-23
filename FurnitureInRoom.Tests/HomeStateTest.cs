b567454d8cb706953ccb5f7117ce89ff51eeacdc
drypa <dr.drypa.k@mail.ru>
2015-06-23 22:36:32 +0300

    home state storage added without implementation

---
+++ FurnitureInRoom.Tests/HomeStateTest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FurnitureInRoom.Tests
+{
+    [TestClass]
+    public class HomeStateTest
+    {
+        //TODO: need implement
+    }
+}
---
+++ FurnitureInRoom/HomeState.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using FurnitureInRoom.BusinessEntities;
+
+namespace FurnitureInRoom
+{
+    public class HomeState
+    {
+        private Dictionary<DateTime, Home> _states;
+        private Dictionary<DateTime, Home> States
+        {
+            get
+            {
+                return _states ?? (_states = new Dictionary<DateTime, Home>());
+            }
+        }
+
+        public void CreateRoom(string roomName, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveRoom(string roomName, string otherRoom, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Room> GetRoomsList(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateFurniture(string furnitureType,string roomName, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveFurniture(string furnitureType, string fromRoomName, string toRoomName, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<DateTime, Home> GetHistory()
+        {
+            throw new NotImplementedException();
+        }
+        public List<DateTime> GetHomeChangeDates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
